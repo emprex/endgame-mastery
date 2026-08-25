4367cf25aa4d2d6ae2438dcd2df1536a2190f206
emprex <guillaume.strohecker@gmail.com>
2026-08-25 10:14:14 +0100

    Add Diagram 1-3 to curriculum

--- lib/features/lessons/data/curriculum.dart
+++ lib/features/lessons/data/curriculum.dart
@@ -6,5 +6,9 @@ import 'pawn_endgame_lessons.dart';
 /// Lessons are added incrementally only after their pedagogical theory,
 /// position data and tests have been verified.
 final List<LessonDefinition> curriculum = List<LessonDefinition>.unmodifiable(
-  <LessonDefinition>[keySquaresLesson01, keySquaresLesson02],
+  <LessonDefinition>[
+    keySquaresLesson01,
+    keySquaresLesson02,
+    keySquaresLesson03,
+  ],
 );
